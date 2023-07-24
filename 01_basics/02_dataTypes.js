b041f56f7fbaa64f5506b6d2e86c780d686c11e1
Subhadip Mishra <113753225+Subhadip77@users.noreply.github.com>
2023-07-24 18:37:42 +0000

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs not browser
+
+//code readability should be high then I don't use ;
+//console.log(3 + 3) 
+//console.log("hiiiiii")
+
+let name = "Subhadip"
+let age = 18
+let isActive = false
+let state = null
+
+//Premitive data type
+//number *limit 2 to power 53
+//bigint 
+//string
+//boolean => true/false
+//null => standalone Value
+//undefined => memory allocate for a variable
+//symbol => for unique component 
+
+//object
+
+console.log(typeof undefined); //o/p undefined
+console.log(typeof null); //object
